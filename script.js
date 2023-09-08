2a7fe5e9d63ddbe080ed5adb1be77abc380964c5
Caleb Princewill Nwokocha <47554663+calebnwokocha@users.noreply.github.com>
2023-09-08 18:14:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch('https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=extracts&titles=Jesus_Christ&exintro')
+   .then(response => response.json())
+   .then(data => {
+       const page = data.query.pages;
+       const content = page[Object.keys(page)[0]].extract;
+       document.getElementById('wikipediaContent').innerHTML = content;
+   });
