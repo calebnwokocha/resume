4b6c1920b8b1d405310626e01fbf02f11bb06a1b
Caleb Princewill Nwokocha <47554663+calebnwokocha@users.noreply.github.com>
2023-10-08 22:56:07 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-<script async
-  src="https://js.stripe.com/v3/buy-button.js">
-</script>
-
-<stripe-buy-button
-  buy-button-id="buy_btn_1NzA1OAa2x4IoFVAJ08K8zPb"
-  publishable-key="pk_live_51Nz8oTAa2x4IoFVAUH1pSUgJ7yfhYHsFBpVIrrJao2HPj8K5B0ShMT2ygwj0z694E08hsCtMLqT060O2uXiIml7c00EX9jjd4I"
->
-</stripe-buy-button>
