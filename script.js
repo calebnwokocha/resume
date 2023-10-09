403395506c79b48ad2fa088fc8ed0be1885dad6e
Caleb Princewill Nwokocha <47554663+calebnwokocha@users.noreply.github.com>
2023-10-08 22:51:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,9 @@
-var stripe = Stripe('YOUR-PUBLISHABLE-KEY');
-var elements = stripe.elements();
-var card = elements.create('card');
-card.mount('#card-element');
+<script async
+  src="https://js.stripe.com/v3/buy-button.js">
+</script>
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const stripe = require('stripe')('YOUR-SECRET-KEY');
-
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.post('/submit-response', async (req, res) => {
-    let aiReason = req.body.ai_reason;
-
-    // TODO: Store the user's reason in your database
-
-    // Process payment
-    let {status} = await stripe.charges.create({
-        amount: 136.63,  // e.g., $10.00, but you'll specify the amount
-        currency: 'cad',
-        description: 'Payment for AI service',
-        source: req.body.stripeToken
-    });
-
-    res.json({status});
-});
-
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
-});
-
+<stripe-buy-button
+  buy-button-id="buy_btn_1NzA1OAa2x4IoFVAJ08K8zPb"
+  publishable-key="pk_live_51Nz8oTAa2x4IoFVAUH1pSUgJ7yfhYHsFBpVIrrJao2HPj8K5B0ShMT2ygwj0z694E08hsCtMLqT060O2uXiIml7c00EX9jjd4I"
+>
+</stripe-buy-button>
