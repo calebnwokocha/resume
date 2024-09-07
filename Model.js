c2a3e25c19e235757ec7196a20b23017eed45ef0
Caleb P. Nwokocha <calebnwokocha@gmail.com>
2024-09-07 18:13:52 -0500

    test

--- Model.js
+++ Model.js
@@ -8,9 +8,11 @@ class Model {
         const latinSquare = this.latinSquare();
         const stochasticValue = Math.floor(Math.random() * latinSquare.length);
         let output = "AI: ";
+        
         for (let value = 0; value < stochasticValue; value++) {
             output += latinSquare[stochasticValue][value] + " ";
         }
+
         return output.trim();
     }
 
@@ -20,10 +22,11 @@ class Model {
         const latinSquare = Array.from({ length: n }, () => Array(n).fill(""));
 
         for (let row = 0; row < n; row++) {
-                for (let col = 0; col < n; col++) {
-                    latinSquare[row][col] = words[(row + col) % n];
-                }
+            for (let col = 0; col < n; col++) {
+                latinSquare[row][col] = words[(row + col) % n];
+            }
         }
+
         return latinSquare;
     }
 }
